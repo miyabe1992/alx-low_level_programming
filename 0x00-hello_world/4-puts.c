fa71ff290645ee1accc174d6b17f1d201af4595c
Abba Muhammad Musa <abbamuhammadmiyabe@gmail.com>
2022-09-11 16:24:17 -0700

    prints exactly Programming is like

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
-/**
-* Main print if the name is postive 
-*
-*Disicription using print
-*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.[D[D[D[* Return 0
-*/
+/**                                                                                                                                     
+
+* Disicription using print                                                                                                     
+
+*                                                                                                                                                                                                                                                   
+
+* Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line
+
+* Return 0                                                                                                                              
+
+*/ 
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+puts ("\"Programming is like building a multilingual puzzle"); 
+return (0); 
 }
