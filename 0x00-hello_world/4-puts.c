d1d7f1a6119c5ad23d440fd670b123f0b603f5e2
Abba Muhammad Musa <abbamuhammadmiyabe@gmail.com>
2022-09-12 02:46:14 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/**
-* Disicription using print
+/** 
+* main - print the string in the put fuction
 *
-* Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line 
-* Return 0 
-*/ 
-int main(void)   
+* Description: using the main fuction  
+* this program prints Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main (void)
 {
-puts ("\"Programming is like building a multilingual puzzle"); 
-return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+chmod u+x 4-puts.c
