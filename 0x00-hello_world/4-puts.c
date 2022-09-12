413a00268de36ccebd8ad1e14ca92cbf64e44e2c
Abba Muhammad Musa <abbamuhammadmiyabe@gmail.com>
2022-09-12 03:52:56 -0700

    prit coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-
-
-
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
+* main - print the string in the put fuction
+*
+* Description: using the main fuction
+* this program prints Programming is like building a multilingual puzzle
+* Return:0  
+*/
+int main (void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
