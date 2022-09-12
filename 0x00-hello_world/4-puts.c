6d729ce38ba65e7a3706f7b9fd0345196bb37d0d
Abba Muhammad Musa <abbamuhammadmiyabe@gmail.com>
2022-09-12 03:10:30 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
-* main - print the string in the put fuction
-*
-* Description: using the main fuction 
-* this program prints Programming is like building a multilingual puzzle
-* Return:0 
-*/
-int main (void)
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+
+	return (0);
+
 }
