0316a8972b21adb4f95e37387fe839ed88c975b3
Abba Muhammad Musa <abbamuhammadmiyabe@gmail.com>
2022-09-12 02:55:34 -0700

    pritout coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-/** 
+/**
 * main - print the string in the put fuction
 *
-* Description: using the main fuction  
+* Description: using the main fuction 
 * this program prints Programming is like building a multilingual puzzle
-* Return: 0
+* Return:0 
 */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0); 
 }
-chmod u+x 4-puts.c
