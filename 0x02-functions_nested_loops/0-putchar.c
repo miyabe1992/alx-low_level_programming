df198b35fbfb80f744d4464e01dcf04c022fa219
Abba Muhammad Musa <abbamuhammadmiyabe@gmail.com>
2022-09-13 04:26:56 -0700

    putcha

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* Main - prints putchar
+* Return: always 0
+*/
+int main (void)
+{
+	_putcha('_');
+	_putcha('p');
+	_putcha('u');
+	_putcha('t');
+	_putcha('c');
+	_putcha('h');
+	_putcha('a');
+return (0);
+}
