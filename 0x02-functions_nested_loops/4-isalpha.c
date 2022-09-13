ee1d8cb3ccc1585ad4adf5c87e344586c6b31c29
Abba Muhammad Musa <abbamuhammadmiyabe@gmail.com>
2022-09-13 07:54:21 -0700

    isola

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,11 @@
-#include "holberton.h"
+#include "main.h" 
 /**
-* _isalpha - Checks for alphabetic character
-*
-*@c: The character to be checked
-* Return: 1 for alphabetic character or 0 for anything else
+* _isalpha - Checks for alphabetic character    
+* @c: The character to be checked  
+* 
+*Return: alphabetic character or 0 for anything else 
 */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
-		return (1);
-	}
-	return (0);
+	return ((c>='a' c<='z')||(c<='A' && c<='Z')); 
 }
